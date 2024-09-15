2831881cbfbf7bbd87d34d4085c094a32c3f99d7
simon-desktop <330771794@qq.com>
2024-09-15 22:08:06 +0800

    [Feature]新增日志悬浮窗 [Todo]搜索算法改进 [Todo]修正页面算法

--- app/src/main/java/fan/akua/hakka/demo/FloatWindow.java
+++ app/src/main/java/fan/akua/hakka/demo/FloatWindow.java
@@ -9,22 +9,49 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 import java.lang.reflect.Field;
 import java.util.Objects;
 
 public class FloatWindow {
     private final Context mContext;
+    private final WindowManager mWindowManager;
     private WindowManager.LayoutParams mWindowParams;
-    private WindowManager mWindowManager;
 
     private View mFloatLayout;
 
+    private Button i, S, E, A, L;
+    private LinearProgressIndicator progress;
+    private TextView logWindow;
+
     public FloatWindow(Context context) {
         this.mContext = context;
+        mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         initFloatWindow();
+        initLogWindow();
     }
 
+    private void initLogWindow() {
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) ?
+                        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY : WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                PixelFormat.TRANSLUCENT);
+
+        logWindow = new TextView(mContext);
+        params.gravity = Gravity.TOP | Gravity.START;
+        mWindowManager.addView(logWindow, params);
+    }
+
+    @SuppressLint("InflateParams")
     private void initFloatWindow() {
         mContext.setTheme(R.style.Theme_HakkaDemo);
         LayoutInflater inflater = LayoutInflater.from(mContext);
@@ -32,8 +59,14 @@ public class FloatWindow {
             return;
         mFloatLayout = inflater.inflate(R.layout.float_view, null);
 
+        i = mFloatLayout.findViewById(R.id.btnI);
+        S = mFloatLayout.findViewById(R.id.btnS);
+        E = mFloatLayout.findViewById(R.id.btnE);
+        A = mFloatLayout.findViewById(R.id.btnA);
+        L = mFloatLayout.findViewById(R.id.btnL);
+        progress = mFloatLayout.findViewById(R.id.progress);
+
         mWindowParams = new WindowManager.LayoutParams();
-        mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         //8.0新特性
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             mWindowParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
@@ -43,6 +76,20 @@ public class FloatWindow {
         mWindowParams.gravity = Gravity.START | Gravity.TOP;
         mWindowParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
         mWindowParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        assert i != null;
+        assert S != null;
+        assert E != null;
+        assert A != null;
+        assert L != null;
+        assert progress != null;
+        progress.hide();
+        i.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progress.show();
+            }
+        });
     }
 
     public void showFloatWindow() {
@@ -58,6 +105,8 @@ public class FloatWindow {
     public void hideFloatWindow() {
         if (mFloatLayout.getParent() != null)
             mWindowManager.removeView(mFloatLayout);
+        if (logWindow.getParent() != null)
+            mWindowManager.removeView(logWindow);
     }
 
 
